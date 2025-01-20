49e775cdaf7ae028ec839fc2528dff3aa32da4b7
Bernardo Veiga <bernardoveiga@hotmail.com>
2025-01-20 16:18:41 +0000

    Rename service - my-cars -> cars

--- src/app/services/car/car.service.spec.ts
+++ src/app/services/car/car.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing'
 
-import { MyCarService } from './my-car.service'
+import { MyCarService } from './car.service'
 
 describe('MyCarService', () => {
   let service: MyCarService
