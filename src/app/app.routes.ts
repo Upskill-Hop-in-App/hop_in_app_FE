0f4a85135e76ec9cd8e15feb8227c5a3f5d75fe9
Luís Pires <joseluiscpires@gmail.com>
2025-01-20 16:38:58 +0000

    ap 11 created my applications component and added cancel application function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LiftComponent } from './components/lift/lift.component';
 import { MyLiftsComponent } from './components/my-lifts/my-lifts.component';
+import { MyApplicationsComponent } from './components/my-applications/my-applications.component';
 
 export const routes: Routes = [
   { path: 'lifts', component: LiftComponent },
   { path: 'my-lifts', component: MyLiftsComponent },
+  { path: 'my-applications', component: MyApplicationsComponent },
 ];
