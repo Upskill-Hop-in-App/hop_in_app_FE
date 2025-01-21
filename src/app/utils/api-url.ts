bde31691ac539f1135046179409de0b5b6652044
garcialucasm <garcia.lucasm@gmail.com>
2025-01-21 14:40:12 +0000

    AP-14 update api url utils

--- src/app/utils/api-url.ts
+++ src/app/utils/api-url.ts
@@ -2,6 +2,7 @@ export const BASE_URL_CARPOOL = 'http://localhost:3000/api/';
 export const BASE_URL_CARS = 'http://localhost:3001/api/';
 
 export enum ApiUrl {
-  register = BASE_URL_CARPOOL + 'auth/register',
+  registerAdmin = BASE_URL_CARPOOL + 'auth/register/admin',
+  registerClient = BASE_URL_CARPOOL + 'auth/register/client',
   login = BASE_URL_CARPOOL + 'auth/login',
 }
