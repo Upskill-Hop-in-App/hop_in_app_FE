1c5391a62cf0adb99ebe3c04f654cdbeb07475d0
garcialucasm <garcia.lucasm@gmail.com>
2025-01-22 00:07:40 +0000

    AP-4 rename and improve main e2e test

--- cypress/e2e/main.e2e.spec.cy.ts
+++ cypress/e2e/main.e2e.spec.cy.ts
@@ -2,12 +2,12 @@ import {
   UserLogin,
   UserRegister,
   UserRoles,
-} from './../../src/app/models/user.model';
+} from './../../src/app/models/user.model'
 describe('Register and Login', () => {
-  const min = 100000;
-  const max = 999999;
+  const min = 100000
+  const max = 999999
 
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
   const adminTest: UserRegister = {
     email: `tobias-${randomNumber}@test.com`,
     name: 'Tobias',
@@ -15,7 +15,7 @@ describe('Register and Login', () => {
     contact: '999999999',
     password: `psw@${randomNumber}`,
     role: UserRoles.admin,
-  };
+  }
 
   const clientTest: UserRegister = {
     email: `tobias-${randomNumber}@test.com`,
@@ -24,7 +24,17 @@ describe('Register and Login', () => {
     contact: '999999999',
     password: `psw@${randomNumber}`,
     role: UserRoles.client,
-  };
+  }
+
+  before(() => {
+    cy.viewport(
+      Cypress.config('viewportWidth'),
+      Cypress.config('viewportHeight')
+    )
+    cy.window().then((win) => {
+      win.resizeTo(screen.width, screen.height)
+    })
+  })
 
   it('Should register a client', () => {
     cy.register(
@@ -32,10 +42,10 @@ describe('Register and Login', () => {
       adminTest.name,
       adminTest.username,
       adminTest.contact,
-      adminTest.password,
-    );
-  });
+      adminTest.password
+    )
+  })
   it('Should login as a client', () => {
-    cy.login(`tobias-${randomNumber}@test.com`, `psw@${randomNumber}`);
-  });
-});
+    cy.login(`tobias-${randomNumber}@test.com`, `psw@${randomNumber}`)
+  })
+})
