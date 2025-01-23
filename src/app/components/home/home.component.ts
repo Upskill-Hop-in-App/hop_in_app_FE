ba53bb7ec7857c47b9058d4f596ee0cccd94213c
garcialucasm <garcia.lucasm@gmail.com>
2025-01-23 02:00:44 +0000

    AP-18 create home component

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { NgOptimizedImage } from '@angular/common'
+
+@Component({
+  selector: 'app-home',
+  imports: [NgOptimizedImage],
+
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {}
