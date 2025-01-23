47b98741fa491132c43df66e8e6dab86d01ae817
garcialucasm <garcia.lucasm@gmail.com>
2025-01-23 01:58:14 +0000

    AP-18 update app component adn app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
-import { Routes } from '@angular/router';
-import { LiftComponent } from './components/lift/lift.component';
-import { MyLiftsComponent } from './components/my-lifts/my-lifts.component';
-import { MyApplicationsComponent } from './components/my-applications/my-applications.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { AdminGuard } from './guards/admin.guard';
-import { UserGuard } from './guards/user.guard';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { MyCarsComponent } from './components/my-cars/my-cars.component';
+import { Routes } from '@angular/router'
+import { LiftComponent } from './components/lift/lift.component'
+import { MyLiftsComponent } from './components/my-lifts/my-lifts.component'
+import { MyApplicationsComponent } from './components/my-applications/my-applications.component'
+import { ProfileComponent } from './components/profile/profile.component'
+import { AdminGuard } from './guards/admin.guard'
+import { UserGuard } from './guards/user.guard'
+import { RegisterComponent } from './components/register/register.component'
+import { LoginComponent } from './components/login/login.component'
+import { MyCarsComponent } from './components/my-cars/my-cars.component'
+import { HomeComponent } from './components/home/home.component'
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'my-cars', component: MyCarsComponent, canActivate: [UserGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
@@ -21,4 +23,4 @@ export const routes: Routes = [
     canActivate: [UserGuard],
   },
   { path: 'profile', component: ProfileComponent },
-];
+]
