15e6bcc4046e7c867227e8bd144d8e13ae20d1a0
Tgbn99 <telmo.nunes@live.com.pt>
2025-01-24 05:13:22 +0000

    Ap-34 bug fix

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -64,7 +64,7 @@ Cypress.Commands.add('getByDataCy', (selector, ...args) => {
 /* -------------------------------------------------------------------------- */
 /* ------------------------------ DEMO FEATURES ----------------------------- */
 /* -------------------------------------------------------------------------- */
-Cypress.Commands.add('demoWait', (time = 10) => {
+Cypress.Commands.add('demoWait', (time = 100) => {
   cy.wait(time)
 })
 
