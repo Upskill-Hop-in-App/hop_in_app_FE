a6741bb45c8435c19b22a03b74c292869f70f6f6
Luís Pires <joseluiscpires@gmail.com>
2025-01-24 11:40:11 +0000

    ap 35 home buttons now take to register and login pages and are hidden when authenticated

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { NgOptimizedImage } from '@angular/common'
+import { AuthService } from '../../services/auth.service'
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,12 @@ import { NgOptimizedImage } from '@angular/common'
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  userToken: string | null = null
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.userToken = this.authService.getToken()
+  }
+}
